31c5a85497be539e4878c7466fa2e6ffa029f0a9
Thibault Schowing <thibault.schowing@heig-vd.ch>
2015-09-18 10:49:17 +0200

    ajout fichier vides 2

---
+++ Projet/AMT_Projet_Untitled/src/main/java/ch/heigvd/amt/amt_projet_untitled/web/filters/NewClass.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.heigvd.amt.amt_projet_untitled.web.filters;
+
+/**
+ *
+ * @author thsch
+ */
+public class NewClass {
+    
+}
