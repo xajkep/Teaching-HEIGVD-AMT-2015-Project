eed54af816092e88f3377dc3e938a3d1eeae2cba
xajkep <xajkep@users.noreply.github.com>
2015-09-27 23:48:39 +0200

    Update AccountServlet.java

--- Projet/AMT_Projet_Untitled/src/main/java/ch/heigvd/amt/amt_project/web/controllers/AccountServlet.java
+++ Projet/AMT_Projet_Untitled/src/main/java/ch/heigvd/amt/amt_project/web/controllers/AccountServlet.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ch.heigvd.amt.amt_project.web.controllers;
 
 import static ch.heigvd.amt.amt_project.web.controllers.AppServlet.LIST_APP;
