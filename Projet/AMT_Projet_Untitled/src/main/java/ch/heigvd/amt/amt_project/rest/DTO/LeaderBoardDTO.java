cfab14a06385926f2e08c7e3515666764e05dc48
yountheory <yountheory@github.com>
2015-11-13 11:18:46 +0100

    LeaderboardDTO maaageule

--- Projet/AMT_Projet_Untitled/src/main/java/ch/heigvd/amt/amt_project/rest/DTO/LeaderBoardDTO.java
+++ Projet/AMT_Projet_Untitled/src/main/java/ch/heigvd/amt/amt_project/rest/DTO/LeaderBoardDTO.java
@@ -1,5 +1,7 @@
 package ch.heigvd.amt.amt_project.rest.DTO;
 
+import java.util.List;
+
 /**
  *
  * @author YounTheory
@@ -7,5 +9,66 @@ package ch.heigvd.amt.amt_project.rest.DTO;
 
 
 public class LeaderBoardDTO {
+    private String firstName;
+    private String lastName;
+    private long points;
+    private List<BadgeDTO> badges;
+
+    /**
+     * @return the firstName
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * @param firstName the firstName to set
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    /**
+     * @return the lastName
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * @param lastName the lastName to set
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    /**
+     * @return the points
+     */
+    public long getPoints() {
+        return points;
+    }
+
+    /**
+     * @param points the points to set
+     */
+    public void setPoints(long points) {
+        this.points = points;
+    }
+
+    /**
+     * @return the badges
+     */
+    public List<BadgeDTO> getBadges() {
+        return badges;
+    }
+
+    /**
+     * @param badges the badges to set
+     */
+    public void setBadges(List<BadgeDTO> badges) {
+        this.badges = badges;
+    }
+    
     
 }
