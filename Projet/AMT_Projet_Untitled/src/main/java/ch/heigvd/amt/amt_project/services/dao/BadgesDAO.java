627615d214b7c694b6eadbf4dfe569da09e164d0
Berthouzoz Michaël <hello@michael-berthouzoz.me>
2015-11-20 10:06:22 +0100

    add Stateless

--- Projet/AMT_Projet_Untitled/src/main/java/ch/heigvd/amt/amt_project/services/dao/BadgesDAO.java
+++ Projet/AMT_Projet_Untitled/src/main/java/ch/heigvd/amt/amt_project/services/dao/BadgesDAO.java
@@ -1,13 +1,14 @@
 package ch.heigvd.amt.amt_project.services.dao;
 
 import ch.heigvd.amt.amt_project.models.Badge;
+import javax.ejb.Stateless;
 
 /**
  *
  * @author thsch
  */
 
-
+@Stateless
 public class BadgesDAO extends GenericDAO<Badge, Long> implements BadgesDAOLocal{
     
 }
