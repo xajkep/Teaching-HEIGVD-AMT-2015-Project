1acae9d7a02bbe00427b66d22d78be16fbf75e9c
Berthouzoz Michaël <hello@michael-berthouzoz.me>
2015-11-20 10:46:54 +0100

    change securityfilter, OK for /api/

--- Projet/AMT_Projet_Untitled/src/main/java/ch/heigvd/amt/amt_project/web/filters/SecurityFilter.java
+++ Projet/AMT_Projet_Untitled/src/main/java/ch/heigvd/amt/amt_project/web/filters/SecurityFilter.java
@@ -73,6 +73,8 @@ public class SecurityFilter implements Filter {
      */
     if (path.startsWith("/static/")) {
       isTargetUrlProtected = false;
+    } else if (path.startsWith("/api/")) {
+      isTargetUrlProtected = false;
     } else if ("/auth".equals(path)) {
       isTargetUrlProtected = false;
     } else {
