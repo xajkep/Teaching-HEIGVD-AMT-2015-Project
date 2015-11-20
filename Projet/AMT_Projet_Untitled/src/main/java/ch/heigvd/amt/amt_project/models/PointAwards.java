17ed642acbc99d44cd442e2ae5d38bd5b7603eb6
Benoît Zuckschwerdt <xajkep@devisland.org>
2015-11-20 09:59:43 +0100

    f

--- Projet/AMT_Projet_Untitled/src/main/java/ch/heigvd/amt/amt_project/models/PointAwards.java
+++ Projet/AMT_Projet_Untitled/src/main/java/ch/heigvd/amt/amt_project/models/PointAwards.java
@@ -1,10 +1,8 @@
 package ch.heigvd.amt.amt_project.models;
 
-import java.util.Date;
+import java.sql.Date;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
-import javax.persistence.Temporal;
-import static javax.persistence.TemporalType.DATE;
 
 /**
  *
@@ -17,8 +15,7 @@ public class PointAwards extends AbstractDomainModel<Long>{
     private EndUser endUser;
     
     private long point;
-    
-    @Temporal(DATE)
+
     private Date date;
     
     private String reason;
