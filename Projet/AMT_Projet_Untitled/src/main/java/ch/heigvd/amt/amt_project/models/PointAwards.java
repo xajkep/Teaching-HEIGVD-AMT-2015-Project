b1004fe3e35d049cd9f1095901eda99bf3a47e79
Benoît Zuckschwerdt <xajkep@devisland.org>
2015-11-20 10:19:26 +0100

    fix: rename Projet/AMT_Projet_Untitled/src/main/setup/sun-resources.xml

--- Projet/AMT_Projet_Untitled/src/main/java/ch/heigvd/amt/amt_project/models/PointAwards.java
+++ Projet/AMT_Projet_Untitled/src/main/java/ch/heigvd/amt/amt_project/models/PointAwards.java
@@ -1,8 +1,10 @@
 package ch.heigvd.amt.amt_project.models;
 
-import java.sql.Date;
+import java.util.Date;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import static javax.persistence.TemporalType.DATE;
 
 /**
  *
@@ -15,7 +17,8 @@ public class PointAwards extends AbstractDomainModel<Long>{
     private EndUser endUser;
     
     private long point;
-
+    
+    @Temporal(DATE)
     private Date date;
     
     private String reason;
