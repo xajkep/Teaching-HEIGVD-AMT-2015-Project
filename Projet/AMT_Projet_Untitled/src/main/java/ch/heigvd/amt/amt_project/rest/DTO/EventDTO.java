25c3ae61c531e56355107d8902869cddf6f951be
yountheory <yountheory@github.com>
2015-11-26 14:48:26 +0100

    push DTOS

--- Projet/AMT_Projet_Untitled/src/main/java/ch/heigvd/amt/amt_project/rest/DTO/EventDTO.java
+++ Projet/AMT_Projet_Untitled/src/main/java/ch/heigvd/amt/amt_project/rest/DTO/EventDTO.java
@@ -1,9 +1,53 @@
 package ch.heigvd.amt.amt_project.rest.dto;
 
+import java.util.List;
+
 /**
  *
  * @author mberthouzoz
  */
 public class EventDTO {
+    private String name;
+    private List<EventPropertiesDTO> properties;
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the properties
+     */
+    public List<EventPropertiesDTO> getProperties() {
+        return properties;
+    }
+
+    /**
+     * @param properties the properties to set
+     */
+    public void setProperties(List<EventPropertiesDTO> properties) {
+        this.properties = properties;
+    }
+     /**
+     * @param propertie the propertie to add
+     */
+    public void addPropertie(EventPropertiesDTO propertie) {
+        this.properties.add(propertie);
+    }
     
+    /**
+     * @param propertie the propertie to delete
+     */
+    public void removePropertie(EventPropertiesDTO propertie) {
+        this.properties.remove(propertie);
+    }
 }
---
+++ Projet/AMT_Projet_Untitled/src/main/java/ch/heigvd/amt/amt_project/rest/DTO/RuleDTO.java
@@ -0,0 +1,12 @@
+package ch.heigvd.amt.amt_project.rest.DTO;
+
+/**
+ *
+ * @author YounTheory
+ */
+
+
+public class RuleDTO {
+    private EventDTO event;
+    
+}
