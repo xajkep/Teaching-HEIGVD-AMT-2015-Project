69acf143eaf8610f8a9950f361ea7de061cfbacb
yountheory <yountheory@github.com>
2015-11-27 11:46:20 +0100

    changement directory dto

--- Projet/AMT_Projet_Untitled/src/main/java/ch/heigvd/amt/amt_project/rest/dto/EventTypeDTO.java
+++ Projet/AMT_Projet_Untitled/src/main/java/ch/heigvd/amt/amt_project/rest/dto/EventTypeDTO.java
@@ -8,7 +8,6 @@ import java.util.List;
  */
 public class EventTypeDTO {
     private String name;
-    private List<RulePropertiesDTO> properties;
 
     /**
      * @return the name
@@ -22,32 +21,5 @@ public class EventTypeDTO {
      */
     public void setName(String name) {
         this.name = name;
-    }
-
-    /**
-     * @return the properties
-     */
-    public List<RulePropertiesDTO> getProperties() {
-        return properties;
-    }
-
-    /**
-     * @param properties the properties to set
-     */
-    public void setProperties(List<RulePropertiesDTO> properties) {
-        this.properties = properties;
-    }
-     /**
-     * @param propertie the propertie to add
-     */
-    public void addPropertie(RulePropertiesDTO propertie) {
-        this.properties.add(propertie);
-    }
-    
-    /**
-     * @param propertie the propertie to delete
-     */
-    public void removePropertie(RulePropertiesDTO propertie) {
-        this.properties.remove(propertie);
     }
 }
--- Projet/AMT_Projet_Untitled/src/main/java/ch/heigvd/amt/amt_project/rest/dto/RuleDTO.java
+++ Projet/AMT_Projet_Untitled/src/main/java/ch/heigvd/amt/amt_project/rest/dto/RuleDTO.java
@@ -1,5 +1,7 @@
 package ch.heigvd.amt.amt_project.rest.dto;
 
+import java.util.List;
+
 /**
  *
  * @author YounTheory
@@ -7,5 +9,19 @@ package ch.heigvd.amt.amt_project.rest.dto;
 
 
 public class RuleDTO {
-    
+    private List<RulePropertiesDTO> properties;
+    private EventTypeDTO eventType;
+    /**
+     * @return the properties
+     */
+    public List<RulePropertiesDTO> getProperties() {
+        return properties;
+    }
+
+    /**
+     * @param properties the properties to set
+     */
+    public void setProperties(List<RulePropertiesDTO> properties) {
+        this.properties = properties;
+    }
 }
