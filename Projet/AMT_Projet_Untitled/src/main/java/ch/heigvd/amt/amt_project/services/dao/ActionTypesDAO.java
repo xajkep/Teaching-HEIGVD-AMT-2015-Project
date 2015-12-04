12e54af6f4344a3f8d4577c152bcfcaa6c992ab3
Benoît Zuckschwerdt <xajkep@devisland.org>
2015-12-04 10:48:55 +0100

    Impl. d'ActionTypesDAO

---
+++ Projet/AMT_Projet_Untitled/src/main/java/ch/heigvd/amt/amt_project/services/dao/ActionTypesDAO.java
@@ -0,0 +1,29 @@
+package ch.heigvd.amt.amt_project.services.dao;
+
+import ch.heigvd.amt.amt_project.models.ActionType;
+import javax.ejb.Stateless;
+import javax.persistence.NoResultException;
+
+/**
+ *
+ * @author xajkep
+ */
+@Stateless
+public class ActionTypesDAO extends GenericDAO<ActionType, Long> implements ActionTypesDAOLocal {
+    
+    public ActionType findByName(String name) 
+            throws BusinessDomainEntityNotFoundException {
+        
+        ActionType result = null;
+        
+        try {
+            result = (ActionType) em.createNamedQuery("ActionType.findByName")
+                    .setParameter("name", name)
+                    .getSingleResult();
+        }  catch (NoResultException e) {
+            throw new BusinessDomainEntityNotFoundException();
+        }
+        
+        return result;
+    }
+}
