7557de8c3114eef8c3046a1e234bc83ed866ee72
Benoît Zuckschwerdt <xajkep@devisland.org>
2015-12-04 18:08:57 +0100

    fix mapping

--- Projet/AMT_Projet_Untitled/src/main/java/ch/heigvd/amt/amt_project/models/BadgeAward.java
+++ Projet/AMT_Projet_Untitled/src/main/java/ch/heigvd/amt/amt_project/models/BadgeAward.java
@@ -1,7 +1,6 @@
 package ch.heigvd.amt.amt_project.models;
 
 import java.util.Date;
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.persistence.NamedQueries;
