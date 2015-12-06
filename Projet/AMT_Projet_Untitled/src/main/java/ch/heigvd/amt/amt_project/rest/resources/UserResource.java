c6ef41e3eda63ef5ae767a49b6e05ba7414e1c5f
Benoît Zuckschwerdt <xajkep@devisland.org>
2015-12-06 16:00:43 +0100

    fix rule add

--- Projet/AMT_Projet_Untitled/src/main/java/ch/heigvd/amt/amt_project/rest/resources/UserResource.java
+++ Projet/AMT_Projet_Untitled/src/main/java/ch/heigvd/amt/amt_project/rest/resources/UserResource.java
@@ -13,8 +13,6 @@ import ch.heigvd.amt.amt_project.services.dao.EndUsersDAOLocal;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.ws.rs.Consumes;
